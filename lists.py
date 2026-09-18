8c2464ad50dec6b0b69dd5fc631a6cbf1bbd82fc
Preetisoni1218 <preetis182007@gmail.com>
2026-09-18 17:55:18 +0530

    Add basic list operations in lists.py

---
+++ lists.py
@@ -0,0 +1,26 @@
+# Codomax Digital Solutions Internship
+# Module 1 - Lists
+
+# Creating a list
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print("Fruits:", fruits)
+
+# Accessing an element
+print("First fruit:", fruits[0])
+
+# Adding an element
+fruits.append("Grapes")
+print("After adding:", fruits)
+
+# Removing an element
+fruits.remove("Banana")
+print("After removing:", fruits)
+
+# Loop through list
+print("All fruits:")
+for fruit in fruits:
+    print(fruit)
+
+# Length of list
+print("Number of fruits:", len(fruits))

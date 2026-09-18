2cfc05fba302fc17aadbbc0c81d135503bd082c6
Preetisoni1218 <preetis182007@gmail.com>
2026-09-18 17:53:37 +0530

    Add functions for greet, add, and square
    Implemented basic functions for greeting, addition, and squaring a number.

---
+++ functions.py
@@ -0,0 +1,21 @@
+# Codomax Digital Solutions Internship
+# Module 1 - Functions
+
+def greet(name):
+    print("Hello", name)
+
+def add(a, b):
+    return a + b
+
+def square(number):
+    return number * number
+
+
+# Calling functions
+greet("Preeti")
+
+result = add(10, 20)
+print("Sum =", result)
+
+num = 5
+print("Square =", square(num))

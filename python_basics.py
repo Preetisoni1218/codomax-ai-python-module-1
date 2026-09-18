0b3e2a359601141a84c00ff45ca62f22f80feeb8
Preetisoni1218 <preetis182007@gmail.com>
2026-09-18 17:46:25 +0530

    Add basic Python script for user input and arithmetic

---
+++ python_basics.py
@@ -0,0 +1,15 @@
+# Codomax Digital Solutions Internship
+# Module 1 - Introduction to AI & Python
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Hello", name)
+print("Your age is", age)
+
+a = 10
+b = 20
+
+print("Sum =", a + b)
+print("Difference =", a - b)
+print("Product =", a * b)

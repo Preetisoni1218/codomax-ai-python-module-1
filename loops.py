849847896dfc5dab7056d0d9a1dab620ce02c5af
Preetisoni1218 <preetis182007@gmail.com>
2026-09-18 17:48:47 +0530

    Implement loops and multiplication table in loops.py
    Added examples of for loop, while loop, and multiplication table.

---
+++ loops.py
@@ -0,0 +1,18 @@
+# Codomax Digital Solutions Internship
+# Module 1 - Loops
+
+print("For Loop:")
+for i in range(1, 6):
+    print(i)
+
+print("\nWhile Loop:")
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
+
+print("\nMultiplication Table:")
+number = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    print(number, "x", i, "=", number * i)

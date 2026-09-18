45b120de3a79516ed671d5bccb782118bfb67986
Preetisoni1218 <preetis182007@gmail.com>
2026-09-18 17:47:52 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,18 @@
+# Codomax Digital Solutions Internship
+# Module 1 - Variables and Data Types
+
+name = "Preeti"
+age = 19
+height = 5.4
+student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", student)
+
+print("\nData Types:")
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(student))
